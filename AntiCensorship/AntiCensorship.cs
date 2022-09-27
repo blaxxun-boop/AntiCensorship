a45df831a1915bfe01508e111d207f54cd8b70bd
blaxxun <blaxxun@blaxxun.blaxxun>
2022-09-27 14:26:23 +0200

    update for latest valheim changes

--- AntiCensorship/AntiCensorship.cs
+++ AntiCensorship/AntiCensorship.cs
@@ -2,30 +2,29 @@
 using BepInEx;
 using HarmonyLib;
 
-namespace AntiCensorship
-{
-	[BepInPlugin(ModGUID, ModName, ModVersion)]
-	public class AntiCensorship : BaseUnityPlugin
-	{
-		private const string ModName = "Anti Censorship";
-		private const string ModVersion = "1.0";
-		private const string ModGUID = "org.bepinex.plugins.anticensorship";
-		
-		public void Awake()
-		{
-			Assembly assembly = Assembly.GetExecutingAssembly();
-			Harmony harmony = new(ModGUID);
-			harmony.PatchAll(assembly);
-		}
+namespace AntiCensorship;
 
-		[HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.CensorShittyWords))]
-		private static class Patch_FejdStartup_CensorShittyWords
+[BepInPlugin(ModGUID, ModName, ModVersion)]
+public class AntiCensorship : BaseUnityPlugin
+{
+	private const string ModName = "Anti Censorship";
+	private const string ModVersion = "1.0.1";
+	private const string ModGUID = "org.bepinex.plugins.anticensorship";
+
+	public void Awake()
+	{
+		Assembly assembly = Assembly.GetExecutingAssembly();
+		Harmony harmony = new(ModGUID);
+		harmony.PatchAll(assembly);
+	}
+
+	[HarmonyPatch(typeof(CensorShittyWords), nameof(CensorShittyWords.Apply))]
+	private static class BypassCensoring
+	{
+		private static bool Prefix(string str, out string __result)
 		{
-			private static bool Prefix(string str, out string __result)
-			{
-				__result = str;
-				return false;
-			}
+			__result = str;
+			return false;
 		}
 	}
 }
